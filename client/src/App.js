bae4cadd01bfea6b68b872e9627950938285e69d
Bank <phanukorn.khankrua@gmail.com>
2022-03-06 12:07:57 +0700

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Navbar, Container } from 'react-bootstrap'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Electionabi from './contracts/Election.json';
+import React, { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Electionabi from "./contracts/Election.json";
 import Web3 from "web3";
+import Navbar from "./Navbar";
+import CandidateTable from "./CandidateTable";
 
 function App() {
-
   useEffect(() => {
-      loadWeb3();
-      LoadBlockchaindata();
-  });
+    loadWeb3();
+    LoadBlockchaindata();
+  }, []);
+
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [loading, setloading] = useState(true);
+  const [ElectionSM, SetElectionSM] = useState();
+  const [data, setData] = useState();
 
   const loadWeb3 = async () => {
     if (window.etherum) {
@@ -21,44 +27,59 @@ function App() {
     } else {
       window.alert("Non etherum browser detect");
     }
-  }
+  };
 
   const LoadBlockchaindata = async () => {
+    setloading(true);
     const web3 = window.web3;
-
-    const accounts = web3.eth.getAccounts();
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
     const account = accounts[0];
-
+    setCurrentAccount(accounts);
+    console.log(currentAccount);
     const networkId = await web3.eth.net.getId();
-
     const networkData = Electionabi.networks[networkId];
 
     if (networkData) {
-      const election = new web3.eth.Contract(Electionabi.abi, networkData.address);
-      console.log(election);
+      const election = new web3.eth.Contract(
+        Electionabi.abi,
+        networkData.address
+      );
+      const candidate1 = await election.methods.candidates(1).call();
+      const candidate2 = await election.methods.candidates(2).call();
+      SetElectionSM(election);
+      let tempData = {
+        candidates: [candidate1, candidate2],
+        contract: election,
+      };
+      setData(tempData);
+      setloading(false);
     } else {
       window.alert("the smartcontract is not deployed current network");
     }
+  };
+
+  async function voteCandidate(candidateId) {
+    try {
+      const message = await ElectionSM.methods.vote(candidateId).call();
+      alert("Vote success");
+    } catch (err) {
+      alert("error");
+    }
   }
 
-  return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            Election Dapp
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </div>
-  );
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    console.log("render");
+    return (
+      <div className="App">
+        <Navbar account={currentAccount}></Navbar>
+        <CandidateTable data={data} callback={voteCandidate}></CandidateTable>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/CandidateTable.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+
+const CandidateTable = ({ data, callback}) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Candidate ID</TableCell>
+            <TableCell align="right">Candidate Name</TableCell>
+            <TableCell align="right">Vote Count</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.candidates.map((candidate, index) => (
+            <TableRow
+              key={candidate.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {candidate.id}
+              </TableCell>
+              <TableCell align="right">{candidate.name}</TableCell>
+              <TableCell align="right">{candidate.voteCount}</TableCell>
+              <TableCell align="left">
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => callback(candidate.id)}
+                >
+                  Success
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+
+
+export default CandidateTable;
